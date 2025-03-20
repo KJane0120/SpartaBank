9904621a3a46845af1e3171d647965c0ae201dcc
KJane0120 <rlawoals5755@gmail.com>
2025-03-20 13:21:51 +0900

    [Feat]3단계 게임매니저 제작

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance { get { return instance; } }
+
+    public UserData data;
+   
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void Start()
+    {
+        data = new UserData("염예찬", 50000, 100000);
+    }
+}
