e1c48427dffecb4e93196501cbc257f010ec6367
KJane0120 <rlawoals5755@gmail.com>
2025-03-20 13:22:09 +0900

    [4단계]데이터와 UI 연동

---
+++ Assets/Scripts/PopUpBank.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PopUpBank : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI userNameText;
+    [SerializeField] private TextMeshProUGUI cashText;
+    [SerializeField] private TextMeshProUGUI balanceText;
+
+    public UserData data;
+    private void Start()
+    {
+        data = new UserData("염예찬", 100000, 50000);
+    }
+
+    private void FixedUpdate()
+    {
+        Refresh();
+    }
+
+    public void Refresh()
+    {
+        userNameText.text = data.userName;
+        cashText.text = string.Format("{0:N0}", data.cash);
+        balanceText.text = string.Format("{0:N0}", data.balance);
+    }
+}
--- Assets/Scripts/UserData.cs
+++ Assets/Scripts/UserData.cs
@@ -1,10 +1,6 @@
-﻿using UnityEngine;
-
+﻿[System.Serializable]
 public class UserData //: MonoBehaviour
 {
-    //[SerializeField] private TextMeshProUGUI userNameText;
-    //[SerializeField] private TextMeshProUGUI cashText;
-    //[SerializeField] private TextMeshProUGUI balanceText;
     public string userName;
     public int cash;
     public int balance;
@@ -15,40 +11,4 @@ public class UserData //: MonoBehaviour
         this.cash = cash;
         this.balance = balance;
     }
-
-
-
-
-    // Start is called before the first frame update
-    //void Start()
-    //{
-    //    userName = "염예찬";
-    //    balance = 50000;
-    //    cash = 100000;
-    //}
-
-    // Update is called once per frame
-    //void Update()
-    //{
-    //    UpdateUserName();
-    //    UpdateCash();
-    //    UpdateBalance();
-    //}
-
-    //string UpdateUserName()
-    //{
-    //    userNameText.text = userName;
-    //    return userNameText.text;
-    //}
-    //string UpdateCash()
-    //{
-    //    cashText.text = string.Format("{0:N0}", cash);
-    //    return cashText.text;
-    //}
-
-    //string UpdateBalance()
-    //{
-    //    balanceText.text = string.Format("{0:N0}", balance);
-    //    return balanceText.text;
-    //}
 }
