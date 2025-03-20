0958da61e2c5f7847510d73410dc5cc267a59ff8
KJane0120 <rlawoals5755@gmail.com>
2025-03-20 20:08:06 +0900

    [Feat] 입/출금 기능 구현
    SerializeField로 선언한 인풋필드는 TMP_InputField로 선언하고 할당해야 한다.
    직접 입력 후 버튼 클릭시 실행되는 함수의 매개변수로 InputField를 받을 수 있다.

--- Assets/Scripts/PopUpBank.cs
+++ Assets/Scripts/PopUpBank.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,8 +10,14 @@ public class PopUpBank : MonoBehaviour
     [SerializeField] private GameObject ATM;
     [SerializeField] private GameObject depositWindow;
     [SerializeField] private GameObject withdrawWindow;
+    [SerializeField] private GameObject popupErrorPanel;
     [SerializeField] private Button depositBtn;
     [SerializeField] private Button withdrawBtn;
+    [SerializeField] private Button customMoney_deposit;
+    [SerializeField] private Button customMoney_withdraw;
+    [SerializeField] private TMP_InputField inputFieldText_deposit;
+    [SerializeField] private TMP_InputField inputFieldText_withdraw;
+
 
     public UserData data { get => GameManager.Instance.data; }
     private void Start()
@@ -37,6 +41,44 @@ public class PopUpBank : MonoBehaviour
         balanceText.text = string.Format("{0:N0}", data.balance);
     }
 
+    public void Deposit(int cash) //입금시
+    {
+        if (data.cash - cash < 0)
+        {
+            popupErrorPanel.SetActive(true);
+            return;
+        }
+
+        data.cash -= cash;
+        data.balance += cash;
+        Refresh();
+    }
+
+    public void Deposit_Input(TMP_InputField input)
+    {
+        int customMoney = int.Parse(inputFieldText_deposit.text.Trim());
+        Deposit(customMoney);
+    }
+
+    public void Withdrawal(int balance) // 출금시 
+    {
+        if (data.balance - balance < 0)
+        {
+            popupErrorPanel.SetActive(true);
+            return;
+        }
+
+        data.balance -= balance;
+        data.cash += balance;
+        Refresh();
+    }
+
+    public void Withdrawal_Input(TMP_InputField input)
+    {
+        int customMoney = int.Parse(inputFieldText_withdraw.text.Trim());
+        Withdrawal(customMoney);
+    }
+
     public void OnClickDepositButton() //ButtonEvent에서 처리했음, 안 쓰는 코드 
     {
         ATM.SetActive(false);
@@ -46,5 +88,18 @@ public class PopUpBank : MonoBehaviour
     {
         ATM.SetActive(false);
         withdrawWindow.SetActive(true);
+    }
+
+    public void OnClickBackButton_Deposit()
+    {
+        inputFieldText_deposit.text = string.Empty;
+        depositWindow.SetActive(false);
+        ATM.SetActive(true);
+    }
+    public void OnClickBackButton_Withdraw()
+    {
+        inputFieldText_withdraw.text = string.Empty;
+        withdrawWindow.SetActive(false);
+        ATM.SetActive(true);
     }
 }
